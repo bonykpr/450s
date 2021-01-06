fd9a77a7ca202a1c374ccc18efc211708a1d61e4
Bony Kumar <58834600+bonykpr@users.noreply.github.com>
2021-01-06 11:00:54 +0530

    Create 44spirally_traversal_of_matrix.cpp

---
+++ 44spirally_traversal_of_matrix.cpp
@@ -0,0 +1,34 @@
+ int row = 0;
+        int col = 0;
+        vector<int> output;
+        
+        while(row<r && col<c)
+        {
+           //Printing 1st row
+           for(int i=col; i<c; i++)
+               output.push_back(matrix[row][i]);
+           row++;
+           
+           //Printing last col
+           for(int i=row; i<r; i++)
+               output.push_back(matrix[i][c-1]);
+           c--;
+           
+           //Printing last row
+            if(row<r)
+            {
+                for(int i=c-1; i>=col; --i)
+                    output.push_back( matrix[r-1][i]);
+                r--;
+            }
+           
+           //Printing first col
+            if(col<c)
+            {
+                for(int i=r-1; i>=row; --i)
+                    output.push_back( matrix[i][col]);
+                col++;    
+            }   
+           
+        }
+        return output;
