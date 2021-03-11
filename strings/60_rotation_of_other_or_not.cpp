a10b820b2914cba8fb721438e1c2e1807dc898eb
Bony Kumar <58834600+bonykpr@users.noreply.github.com>
2021-03-11 21:48:57 +0530

    Create 60_rotation_of_other_or_not.cpp

---
+++ strings/60_rotation_of_other_or_not.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool AreRotations(string str1, string str2)
+{
+    if(str1.length()!=str2.length())
+        return false;
+        
+    string temp=str1+str1;
+    return (temp.find(str2)!=string::npos);
+}
+
+int main() {
+	//code
+	string str1="AACD", str2="ACDA";
+	if(AreRotations(str1,str2))
+	    cout<<"Strings are rotation";
+	   else
+	    cout<<"NO";
+	return 0;
+}
